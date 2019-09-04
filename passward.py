ac7f5316a7a853026143a464208fb565a81fe880
Brian Lin <yoyoboy323@gmail.com>
2019-09-04 15:43:46 -0400

    basic version done

---
+++ passward.py
@@ -0,0 +1,12 @@
+passward = 'brian79712'
+i = 3
+while True:
+	pwd = input('請輸入密碼: ')
+	if pwd == passward: 
+		print('登入成功！')
+		break
+	else:
+		i = i - 1
+		print('密碼錯誤! 還有' , i, '次機會!')
+		if i == 0:
+			break
